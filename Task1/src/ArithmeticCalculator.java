75b6474175f11f2bb05443522f994ee3190c514e
T1melord1 <vlad.bobrenok.2001@gmail.com>
2024-01-25 17:53:17 +0300

    Реализуйте конструктор с двумя параметрами-числами, который сохраняет переданные числа в свойствах класса.     //2.Реализуйте метод calculate, на вход которого передаётся одна из арифметических операций     // (объект класса Operation — смотрите следующий пункт задания), которую нужно произвести с двумя числами,     // переданными в конструктор при создании объекта.     //3.Создайте Enum Operation с тремя значениями: ADD, SUBTRACT, MULTIPLY. Эти значения будут     // соответствовать арифметическим операциям — сложению, вычитанию и умножению.

---
+++ Task1/src/ArithmeticCalculator.java
@@ -0,0 +1,33 @@
+public class ArithmeticCalculator {
+
+    //todo
+    // Создайте класс ArithmeticCalculator, который будет выполнять арифметические операции над числами.
+    //1.Реализуйте конструктор с двумя параметрами-числами, который сохраняет переданные числа в свойствах класса.
+    //2.Реализуйте метод calculate, на вход которого передаётся одна из арифметических операций
+    // (объект класса Operation — смотрите следующий пункт задания), которую нужно произвести с двумя числами,
+    // переданными в конструктор при создании объекта.
+    //3.Создайте Enum Operation с тремя значениями: ADD, SUBTRACT, MULTIPLY. Эти значения будут
+    // соответствовать арифметическим операциям — сложению, вычитанию и умножению.
+    private int first;
+    private int second;
+
+    public ArithmeticCalculator(int first, int second) {
+        this.first = first;
+        this.second = second;
+    }
+
+    public int calculate(Operation type) {
+        if (type == Operation.ADD) {
+            return first + second;
+        } else if (type == Operation.SUBTRACT) {
+            return first - second;
+        } else if (type == Operation.MULTIPLY) {
+            return first * second;
+        }
+          else {
+              throw new IllegalArgumentException("Invalid operation");
+        }
+    }
+    }
+
+
--- Task1/src/Main.java
+++ Task1/src/Main.java
@@ -1,5 +1,8 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Test");
+        ArithmeticCalculator calculator = new ArithmeticCalculator(5,10);
+        System.out.println(calculator.calculate(Operation.ADD));
+
+
     }
 }

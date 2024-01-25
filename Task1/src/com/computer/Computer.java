593b2556daefefcce1ee002a449caf8c460d98c9
T1melord1 <vlad.bobrenok.2001@gmail.com>
2024-01-25 21:17:32 +0300

    Создание свойства-компонентов

--- Task1/src/com/computer/Computer.java
+++ Task1/src/com/computer/Computer.java
@@ -1,6 +1,10 @@
 package com.computer;
 
 public class Computer {
-
+    private Processor processor;
+    private AccessMemory accessMemory;
+    private HardDisk hardDisk;
+    private Screen screen;
+    private Keyboard keyboard;
 
 }
